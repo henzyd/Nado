1e684c8c7511be0da01f5afc6ed0c93dacdea25b
Henzyd <henzydee@gmail.com>
2023-04-01 21:33:18 +0100

    Built the home layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Logo from "./Logo";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const navData = [
+    {
+      id: 1,
+      name: "Buy",
+      link: "/buy",
+    },
+    {
+      id: 2,
+      name: "Sell",
+      link: "/sell",
+    },
+  ];
+  return (
+    <div id="NavBar" className=" w-full flex justify-between items-center">
+      <Logo />
+
+      <div>
+        {navData.map((item) => (
+          <NavLink key={item.id} to={item.link}>
+            {item.name}
+          </NavLink>
+        ))}
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default NavBar;
