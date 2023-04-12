a16b3bf2893f80c980bfd7518fe68cf9000050b8
Henzyd <henzydee@gmail.com>
2023-04-12 16:21:49 +0100

    Created the skeleton of the lorem page

---
+++ src/Pages/Home/Lorem.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Lorem = () => {
+  return (
+    <div>
+      <div className="w-full">
+        <h1>Lorem</h1>
+        <main className=" bg-transparent">
+          <div className="relative">
+            <div className="bg-[#CECECE] absolute"></div>
+            <h2>Lorem Ipsum</h2>
+            <p>Something very long Like a long body text...</p>
+            <div className="bg-white flex justify-center items-center">
+              <h4>Image</h4>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="bg-[#CECECE] absolute"></div>
+            <h2>Lorem Ipsum</h2>
+            <p>Something very long Like a long body text...</p>
+            <div className="bg-white flex justify-center items-center">
+              <h4>Image</h4>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Lorem;
