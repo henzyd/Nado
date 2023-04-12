b202b8a7f84640f49d03115397e5a9875a748b9d
Henzyd <henzydee@gmail.com>
2023-04-12 15:37:30 +0100

    Created the top items section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,12 @@ export default {
       backgroundColor: {
         Button: "#B6B6B6",
         HomeIntro: "#949494",
+        HomeTopItemsNew: "#989898",
       },
       gridTemplateColumns: {
         HomeIntroSearch: "1fr 0.1fr 1fr 0.5fr",
         HomeSlider: `repeat(auto-fill, minmax(100px, 1fr))`,
+        HomeTopItems: `repeat(auto-fill, minmax(200px, 1fr))`,
       },
     },
   },
